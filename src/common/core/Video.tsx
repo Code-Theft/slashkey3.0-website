59d1534758d820790833391edd90ef9f2da4d50d
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-04-14 12:54:20 +0530

    feat: removed comments

--- src/common/core/Video.tsx
+++ src/common/core/Video.tsx
@@ -14,12 +14,6 @@ export default function Video({ className }: { className: string }) {
       className={className}
     >
       <source src="/home.mp4" type="video/mp4" />
-      {/* <track
-        src="/path/to/captions.vtt"
-        kind="subtitles"
-        srcLang="en"
-        label="English"
-      /> */}
       Your browser does not support the video tag.
     </video>
   );
