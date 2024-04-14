7fa58dbdc0efadadbab24d62967c8d1885fd4498
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-04-14 12:10:19 +0530

    build: added next-video

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+const path = require("path");
+const withVideos = require("next-videos");
 
 const nextConfig = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
       },
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-      },
+    ],
+  },
 };
 
-
 module.exports = nextConfig;
+module.exports = withVideos();
