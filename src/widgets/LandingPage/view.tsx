6ae3a7f987546339a3674cfa46ed2e4d45872941
JANAJITH D <56438939+Code-Theft@users.noreply.github.com>
2024-04-14 15:29:43 +0530

    feat: added video landing

--- src/widgets/LandingPage/view.tsx
+++ src/widgets/LandingPage/view.tsx
@@ -15,7 +15,7 @@ export default function LandingPageview() {
   return (
     <>
       <div className="bg-slk-black-200 relative overflow-hidden w-full min-h-screen">
-        {/* <Video className="absolute inset-0 w-full h-full object-cover" /> */}
+        <Video className="absolute inset-0 w-full h-full object-cover" />
         <div className="container m-auto px-6 py-20 md:pb-0 md:pt-40 md:px-12 lg:py-0 lg:px-10 relative z-10">
           <HeroView />
         </div>
